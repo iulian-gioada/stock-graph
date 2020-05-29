7be1a5d7379bc307f0ba4726673f6631782b40c1
Iulian Gioada <iulian@MacBook-Pro-5.local>
2020-05-29 20:12:30 +0300

    Updates

---
+++ precache-manifest.d23df5d61f1c1451916775784600bcae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d860009e4c860a435a9770c35350f5ef",
+    "url": "/stock-graph/index.html"
+  },
+  {
+    "revision": "5f9b7d2e9c921921a27d",
+    "url": "/stock-graph/static/css/main.6b0b2208.chunk.css"
+  },
+  {
+    "revision": "99b9467b3eb06ddac4bd",
+    "url": "/stock-graph/static/js/2.47fbf000.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/stock-graph/static/js/2.47fbf000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9b7d2e9c921921a27d",
+    "url": "/stock-graph/static/js/main.a33e35ae.chunk.js"
+  },
+  {
+    "revision": "09b952021e7439f841ef",
+    "url": "/stock-graph/static/js/runtime-main.b02d8e86.js"
+  }
+]);
